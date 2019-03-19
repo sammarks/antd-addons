ace544775b185ca80fc7a51f7766a0330ceb678a
Sam Marks <sam@sammarks.me>
2019-03-19 12:18:37 -0400

    chore: Remove debug.

--- build.js
+++ build.js
@@ -7,7 +7,6 @@ const babelPluginImportConfig = {
   camel2DashComponentName: false,
   style: (name) => {
     const stylePath = path.resolve('node_modules', `${name}.css`)
-    console.log(stylePath)
     if (fs.existsSync(stylePath)) {
       return `${name}.css`
     } else return false
